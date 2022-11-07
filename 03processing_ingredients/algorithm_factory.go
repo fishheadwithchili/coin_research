2f8c6c0736eb0ed218ddf578265c4ab48ab78e86
fishheadwithchili <fishheadwithchili@gmail.com>
2022-11-07 14:22:38 +1300

    [try]要提交上去才能引本地包？

---
+++ 03processing_ingredients/algorithm_factory.go
@@ -0,0 +1,11 @@
+package processing_ingredients
+
+type Algo interface {
+	// GetCurrentValueToInt64 get current value after algorithm
+	// param previousResults:the previous algorithm results
+	// param currentOriginValue: the current origin value before algorithm
+	// return current result after algorithm
+	GetCurrentValueToInt64(previousResults []int64, currentOriginValue int64) int64
+}
+
+func TT() {}
