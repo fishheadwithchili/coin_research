812a2618cd10e37a6026aceab2e679845337190c
fishheadwithchili <fishheadwithchili@gmail.com>
2022-11-08 02:07:20 +1300

    [try]尝试

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/fishheadwithchili/coin_research/steps/01obtaining_ingredients"
+	"github.com/fishheadwithchili/coin_research/steps/03processing_ingredients"
 )
 
 func main() {
 	fmt.Println("start")
+	data := obtaining_ingredients.BaseData()
+	algo := processing_ingredients.Algo(data)
 }
